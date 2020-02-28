392854c5efabb233ac81ed4ac4777e250c12a62e
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2020-02-28 01:36:10 +0100

    Updates

---
+++ precache-manifest.14547b44cf2a4b90d35aee1699cf7531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9674c815ab7a711f20b03384bde277",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9aa835c61407c9977957",
+    "url": "/portfolio/static/css/main.9c6cdf50.chunk.css"
+  },
+  {
+    "revision": "81372f0bcf9ca7ad2b18",
+    "url": "/portfolio/static/js/2.0255cb39.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.0255cb39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa835c61407c9977957",
+    "url": "/portfolio/static/js/main.73433649.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
