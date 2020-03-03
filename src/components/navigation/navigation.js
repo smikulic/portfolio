249bce6489a0b0f74e4c195cf57b16b7ac821d5b
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2020-03-04 00:28:44 +0100

    Add hash router

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import { HashLink } from 'react-router-hash-link'
 import './navigation.css'
 
 function Navigation() {
@@ -7,9 +8,11 @@ function Navigation() {
     <nav className="navigation">
       <div className="items">
         <Link to="/" className="item">Home</Link>
-        <a href="/#work" className="item">Work</a>
+        <HashLink to="/#work" className="item">Work</HashLink>
+        {/* <a href="/#work" className="item">Work</a> */}
         <Link to="/about" className="item">About</Link>
-        <a href="/#contact" className="item">Get in touch</a>
+        <HashLink to="/#contact" className="item">Get in touch</HashLink>
+        {/* <a href="/#contact" className="item">Get in touch</a> */}
       </div>
     </nav>
   );
