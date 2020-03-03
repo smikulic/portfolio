9a2633a75a67bfe991ec63c7796f7dea21864488
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2020-03-04 00:07:31 +0100

    Updates

--- precache-manifest.c66179d206061174ea56e4328c1a3347.js
+++ precache-manifest.c66179d206061174ea56e4328c1a3347.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f3394de623c19ccc3287a7800e6f2558",
+    "revision": "d2f8860c6abe288918f865b1cdf2b573",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "d472ac8dae36ddc01b4d",
-    "url": "/portfolio/static/css/main.018d92a4.chunk.css"
+    "revision": "90d334c0c94bb205edad",
+    "url": "/portfolio/static/css/main.720d95f1.chunk.css"
   },
   {
-    "revision": "9571e5721b78d80ad278",
-    "url": "/portfolio/static/js/2.c9bae9ef.chunk.js"
+    "revision": "452cb80c808525b2ffa1",
+    "url": "/portfolio/static/js/2.6c9e9591.chunk.js"
   },
   {
-    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/portfolio/static/js/2.c9bae9ef.chunk.js.LICENSE.txt"
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.6c9e9591.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d472ac8dae36ddc01b4d",
-    "url": "/portfolio/static/js/main.b4628a05.chunk.js"
+    "revision": "90d334c0c94bb205edad",
+    "url": "/portfolio/static/js/main.bb290ca2.chunk.js"
   },
   {
     "revision": "0d98471f6c3b69ca4251",
     "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "3d095f584ca0618800b24a70e063164c",
+    "url": "/portfolio/static/media/credit-karma-project.3d095f58.webp"
   },
   {
     "revision": "6614c8bc3b85d52affa9d2a8f9a6d1f3",
@@ -32,12 +36,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/ivor.d453d065.webp"
   },
   {
+    "revision": "cca1eda758919352c294acce5b7a09b6",
+    "url": "/portfolio/static/media/movieo-project.cca1eda7.webp"
+  },
+  {
     "revision": "0a363d20d2e73d5aa5c01e2b15c87872",
     "url": "/portfolio/static/media/profile.0a363d20.webp"
   },
   {
     "revision": "b88fae414c352a8e024a280264f1544e",
     "url": "/portfolio/static/media/scroogevault-showcase-opt.b88fae41.webp"
+  },
+  {
+    "revision": "8c7847627cfd8c51fb2707bc49e10b59",
+    "url": "/portfolio/static/media/solarisbank-project.8c784762.webp"
   },
   {
     "revision": "2170fc1b3398268ecbdf69276101d942",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.134f80d5f18105ce28cd91757d2f0bf2.js"
+  "/portfolio/precache-manifest.c66179d206061174ea56e4328c1a3347.js"
 );
 
 self.addEventListener('message', (event) => {
