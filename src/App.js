d7814f36dcc1ae26ddfb946c7dae0ac9f7cc4f6c
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2020-03-04 00:06:57 +0100

    Add about page and polish up responsiveness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
 import Navigation from './components/navigation/navigation'
 import Footer from './components/footer/footer'
 import SectionIntro from './sections/section-intro/section-intro'
@@ -7,6 +12,7 @@ import SectionShowcase from './sections/section-showcase/section-showcase'
 import SectionHowIWork from './sections/section-how-i-work/section-how-i-work'
 import SectionContact from './sections/section-contact/section-contact'
 import SectionWork from './sections/section-work/section-work'
+import SectionAbout from './sections/section-about/section-about'
 import './App.css'
 
 // EBDE37
@@ -22,16 +28,26 @@ function App() {
   return (
     <div className="App">
       <main className="container">
+
+      <Router>
         <Navigation />
-
-        <SectionIntro />
-        <SectionTestimonials />
-        <SectionWork />
-        <SectionShowcase />
-        <SectionHowIWork />
-        <SectionContact />
-
+        
+        <Switch>
+          <Route exact path="/">
+            <SectionIntro />
+            <SectionTestimonials />
+            <SectionWork />
+            <SectionShowcase />
+            <SectionHowIWork />
+            <SectionContact />
+          </Route>
+          <Route path="/about">
+            <SectionAbout />
+          </Route>
+        </Switch>
+        
         <Footer />
+      </Router>
       </main>
     </div>
   );
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './navigation.css';
+import React from 'react'
+import { Link } from "react-router-dom"
+import './navigation.css'
 
 function Navigation() {
   return (
@@ -7,6 +8,7 @@ function Navigation() {
       <div className="items">
         <a href="/" className="item">Home</a>
         <a href="/#work" className="item">Work</a>
+        <Link to="/about" className="item">About</Link>
         <a href="/#contact" className="item">Get in touch</a>
       </div>
     </nav>
