2aa00378a2d665b4f9e750a82c1b04e1223b163b
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2021-08-29 13:55:10 +0200

    Add section images

--- src/sections/section-glean/section-glean.js
+++ src/sections/section-glean/section-glean.js
@@ -1,4 +1,13 @@
 import React from "react";
+import gleanHomepageImg from "../../images/glean-homepage.webp";
+import gleanHomepageImgJpg from "../../images/glean-homepage.jpg";
+import gleanCompareImg from "../../images/glean-compare.webp";
+import gleanCompareImgJpg from "../../images/glean-compare.jpg";
+import gleanPaymentOneImg from "../../images/glean-payment-1.webp";
+import gleanPaymentOneImgJpg from "../../images/glean-payment-1.jpg";
+import gleanPaymentTwoImg from "../../images/glean-payment-2.webp";
+import gleanPaymentTwoImgJpg from "../../images/glean-payment-2.jpg";
+import "./section-glean.css";
 
 function SectionGlean() {
   return (
@@ -34,7 +43,61 @@ function SectionGlean() {
           expectations
         </b>
       </p>
-      <b>Changed team communication</b>
+      <b>1. Added key features towards our product stability and value</b>
+      <p>
+        Payment processing
+        <br />- enabled our <b>revenue stream</b> and Plaid integration
+        <div className="section-img-two-column">
+          <div className="section-img">
+            <picture className="section-img-center">
+              <source
+                srcSet={gleanPaymentOneImg}
+                type="image/webp"
+                alt="Glean Payment"
+              />
+              <img src={gleanPaymentOneImgJpg} alt="Glean Payment" />
+            </picture>
+          </div>
+          <div className="section-img">
+            <picture className="section-img-center">
+              <source
+                srcSet={gleanPaymentTwoImg}
+                type="image/webp"
+                alt="Glean Payment"
+              />
+              <img src={gleanPaymentTwoImgJpg} alt="Glean Payment" />
+            </picture>
+          </div>
+        </div>
+        New Homepage dashboard and vendor spend comparison
+        <br />- it gave our users more actionable invoice and vendor analysis
+        <div className="section-img-two-column">
+          <div className="section-img">
+            <picture className="section-img-center">
+              <source
+                srcSet={gleanHomepageImg}
+                type="image/webp"
+                alt="Glean Homepage"
+              />
+              <img src={gleanHomepageImgJpg} alt="Glean Homepage" />
+            </picture>
+          </div>
+          <div className="section-img">
+            <picture className="section-img-center">
+              <source
+                srcSet={gleanCompareImg}
+                type="image/webp"
+                alt="Glean Compare"
+              />
+              <img src={gleanCompareImgJpg} alt="Glean Compare" />
+            </picture>
+          </div>
+        </div>
+        More granular approach to building components
+        <br />- increased our development velocity and cross-team understanding
+        of the code that reduced "knowledge silos"
+      </p>
+      <b>2. Changed team communication</b>
       <p>
         By having weekly team sync we managed to establish certain engineering
         standards that helped out our code review process significantly.
@@ -42,7 +105,7 @@ function SectionGlean() {
         We started documeting our best practices and created a technical roadmap
         that would guide our decision making process and ensure code quality.
       </p>
-      <b>Pushed for engineers to own product feature discovery process</b>
+      <b>3. Pushed for engineers to own product feature discovery process</b>
       <p>
         By assigning specific feature owner we enabled engineers to ask the
         right questions and discover potential risks to development ahead of
@@ -52,21 +115,6 @@ function SectionGlean() {
         We started giving relevant feedback to our product managers in order to
         reduce the scope of potential "rabbit holes" or split the feature into
         MVP and Nice To Have items.
-      </p>
-      <b>Added key features towards our product stability and value</b>
-      <p>
-        Payment processing - enabled our <b>revenue stream</b> and Plaid
-        integration
-        <br />
-        New Homepage dashboard - it gave our users more actionable invoice and
-        vendor analysis
-        <br />
-        Development testing strategy - improved our code stability and quicker
-        bug detection
-        <br />
-        More granular approach to building components - increased our
-        development velocity and cross-team understanding of the code that
-        reduced "knowledge silos"
       </p>
       <br />
       <p className="prominent">
